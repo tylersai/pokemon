8457ba44250dd0e25053359e553e15b5106c8570
Sai Han <thesaihan@gmail.com>
2022-02-12 13:36:40 +0630

    Add clickable link in nav logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { FC } from "react";
 import classNames from "classnames";
 import styles from "../styles/Navbar.module.scss";
+import Link from "next/link";
 
 export const Navbar: FC = () => {
   return (
@@ -9,8 +10,17 @@ export const Navbar: FC = () => {
       className={classNames(styles.Navbar, "d-flex align-items-center justify-content-between")}
     >
       <div className="d-flex align-items-center">
-        <Image src="/pokemon.png" alt="Logo" role="img" width={46} height={46} />
-        <h1 className="mb-0 mt-1 ms-3 ms-lg-4 text-primary">Pokemon</h1>
+        <Link href="/">
+          <a className="text-decoration-none">
+            <Image src="/pokemon.png" alt="Logo" role="img" width={46} height={46} />
+          </a>
+        </Link>
+
+        <Link href="/">
+          <a className="text-decoration-none">
+            <h1 className="mb-0 mt-1 ms-3 ms-lg-4 text-primary">Pokemon</h1>
+          </a>
+        </Link>
       </div>
       <button className={styles.menuButton}>
         <i className="bi bi-list"></i>
