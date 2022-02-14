7b63338b7d05d078055751466387b8613f02a010
Sai Han <thesaihan@gmail.com>
2022-02-14 16:02:55 +0630

    Show Filter indicators and clear filter feature

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -30,6 +30,7 @@ interface FilterProps {
 }
 
 export const Filter: FC<FilterProps> = ({
+  name,
   setName,
   cardType,
   setCardType,
@@ -111,61 +112,107 @@ export const Filter: FC<FilterProps> = ({
   );
 
   return (
-    <div className="d-flex justify-content-center py-2">
-      <div
-        className={classNames(
-          styles.filterGroup,
-          "d-flex justify-content-center align-items-center ps-2 pe-1",
-        )}
-      >
-        <input
-          className={classNames(styles.borderRight, "form-control text-secondary")}
-          type="text"
-          placeholder="Name..."
-          value={_name}
-          onChange={(e) => _setName(e.target.value)}
-          onKeyUp={onNameKeyUp}
-        />
-        <select
-          className={classNames(styles.borderRight, "form-select text-secondary")}
-          placeholder="Type"
-          value={cardType}
-          onChange={onTypeChange}
+    <>
+      <div className="d-flex justify-content-center py-2">
+        <div
+          className={classNames(
+            styles.filterGroup,
+            "d-flex justify-content-center align-items-center ps-2 pe-1",
+          )}
         >
-          <option value="">Type</option>
-          {types.map((el) => (
-            <option key={el} value={el}>
-              {el}
-            </option>
-          ))}
-        </select>
-        <select
-          className={classNames(styles.borderRight, "form-select text-secondary")}
-          placeholder="Rarity"
-          value={rarity}
-          onChange={onRarityChange}
-        >
-          <option value="">Rarity</option>
-          {rarities.map((el) => (
-            <option key={el} value={el}>
-              {el}
-            </option>
-          ))}
-        </select>
-        <select
-          className="form-select text-secondary"
-          placeholder="Set"
-          value={cardSet}
-          onChange={onSetChange}
-        >
-          <option value="">Set</option>
-          {sets.map((el) => (
-            <option key={el?.id} value={el?.id}>
-              {el?.name}
-            </option>
-          ))}
-        </select>
+          <input
+            className={classNames(styles.borderRight, "form-control text-secondary")}
+            type="text"
+            placeholder="Name..."
+            value={_name}
+            onChange={(e) => _setName(e.target.value)}
+            onKeyUp={onNameKeyUp}
+          />
+          <select
+            className={classNames(styles.borderRight, "form-select text-secondary")}
+            placeholder="Type"
+            value={cardType}
+            onChange={onTypeChange}
+          >
+            <option value="">Type</option>
+            {types.map((el) => (
+              <option key={el} value={el}>
+                {el}
+              </option>
+            ))}
+          </select>
+          <select
+            className={classNames(styles.borderRight, "form-select text-secondary")}
+            placeholder="Rarity"
+            value={rarity}
+            onChange={onRarityChange}
+          >
+            <option value="">Rarity</option>
+            {rarities.map((el) => (
+              <option key={el} value={el}>
+                {el}
+              </option>
+            ))}
+          </select>
+          <select
+            className="form-select text-secondary"
+            placeholder="Set"
+            value={cardSet}
+            onChange={onSetChange}
+          >
+            <option value="">Set</option>
+            {sets.map((el) => (
+              <option key={el?.id} value={el?.id}>
+                {el?.name}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
-    </div>
+      {(name || cardType || cardSet || rarity) && (
+        <div className="d-flex justify-content-center pb-2">
+          <div
+            className={classNames(
+              styles.filterIndicatorWrapper,
+              "d-flex align-items-center px-3 flex-wrap",
+            )}
+          >
+            {name && name.trim() && (
+              <span className="px-2 py-1 text-secondary text-nowrap letter-spacing-1">
+                Name: {name.trim()}
+              </span>
+            )}
+            {cardType && (
+              <span className="px-2 py-1 text-secondary text-nowrap letter-spacing-1">
+                Type: {cardType}
+              </span>
+            )}
+            {rarity && (
+              <span className="px-2 py-1 text-secondary text-nowrap letter-spacing-1">
+                Rarity: {rarity}
+              </span>
+            )}
+            {cardSet && (
+              <span className="px-2 py-1 text-secondary text-nowrap letter-spacing-1">
+                Set: {sets.find((el) => el.id === cardSet)?.name}
+              </span>
+            )}
+            <span
+              className="px-2 py-1 text-danger text-nowrap letter-spacing-1 text-uppercase"
+              role="button"
+              onClick={() => {
+                _setName("");
+                setName("");
+                setCardType("");
+                setRarity("");
+                setCardSet("");
+              }}
+            >
+              <i className="bi bi-x-lg"></i> Clear
+            </span>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
